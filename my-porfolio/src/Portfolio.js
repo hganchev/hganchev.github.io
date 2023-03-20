3ba22f14c3bfcc469ce3f295aa493cbe7b2d92a0
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2023-03-20 14:51:52 +0200

    first

---
+++ my-porfolio/src/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Portfolio() {
+  return (
+    <div>
+      <h1>My Portfolio</h1>
+      <p>Welcome to my portfolio site!</p>
+    </div>
+  );
+}
+
+export default Portfolio;
