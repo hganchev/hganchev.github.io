629e9121f41463ea62aa189a08cd4d4c80e981d4
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2023-03-31 14:18:04 +0300

    add: Mision

--- my-porfolio/src/Mission/Mission.js
+++ my-porfolio/src/Mission/Mission.js
@@ -4,7 +4,12 @@ import './Mission.css';
 function Mission() {
   return (
     <div id='mission-page'>
-          
+          <p>
+            To leverage my experience in production engineering, automation engineering, 
+            and software design engineering to drive impactful technological advancements 
+            in the tech industry, while also utilizing my enthusiasm for 3D printing and 
+            programming to innovate and create new solutions.
+          </p>
     </div>
   );
 }
