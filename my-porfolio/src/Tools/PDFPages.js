297f912e8f79e3830117aa00446f3466a6fd11dc
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2023-03-31 14:50:15 +0300

    ++

--- my-porfolio/src/Tools/PDFPages.js
+++ my-porfolio/src/Tools/PDFPages.js
@@ -1,10 +1,9 @@
 import React, { useState, Component } from 'react';
 // import { Document, Page, pdfjs } from "react-pdf";
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import { Document, Page, pdfjs } from 'react-pdf/dist/esm/entry.webpack';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
-
 import "./PDFPages.css"
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const slideStyles = {
 	width: "100%",
