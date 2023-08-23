9384414450029db8102531d745596d16eb3e307d
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2023-08-23 12:36:15 +0300

    change the place for projects in utils

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, Component, useEffect }  from 'react';
 import styled from "styled-components";
-import SlideControl from "../../components/SlideControl";
 import './Projects.css';
 
-import BachlorThesis from "./thesis/BThesis_EN.pdf"
-import MasterThesis from "./thesis/MThesis_EN.pdf"
-
 function Projects() {
+  const [pdfFiles, setPdfFiles] = useState(["../thesis/BThesis_EN.pdf"]);
+
+  const searchForPDFFiles = async (folderPath) => {
+    try {
+      const filesData = [
+        { name: 'BThesis_EN.pdf', type: 'application/pdf', url: '../thesis/BThesis_EN.pdf' },
+        { name: 'MThesis_EN.pdf', type: 'application/pdf', url: '../thesis/MThesis_EN.pdf' }
+        // Add more files as needed
+      ];
+      const pdfFilesData = filesData.filter(file => file.type === 'application/pdf');
+      
+      const newPdfFiles = pdfFilesData.map(fileData => ({
+        name: fileData.name,
+        url: fileData.url
+      }));
+      
+      setPdfFiles(newPdfFiles);
+    } catch (error) {
+      console.error('Error fetching and processing files:', error);
+    }
+  };
+
+  useEffect(() => {
+    searchForPDFFiles('../thesis/'); // Replace with your backend API endpoint
+  }, []);
+
   return (
     <div className='projects'>
       <div className='section_title al_center'>
@@ -14,18 +36,13 @@ function Projects() {
         <h1 className='section_title_text s_40'> Projects </h1>
       </div>
       <div class="grid">
-        <div class="item"> 
-          <SlideControl fileUrl={BachlorThesis}
-                        title='Bachlor Thesis'/>
+      {pdfFiles.map((file, index) => (
+        <div class="item" key={index}>
+          <a href={file.url} target="_blank" rel="noopener noreferrer">
+            <img src={'../thesis/HIG_IMG.jpg'}/>
+          </a>
         </div>
-        <div class="item">           
-          <SlideControl fileUrl={MasterThesis}
-                        title='Master Thesis' />
-        </div>
-        <div class="item"> project 3 </div>
-        <div class="item"> project 4 </div>
-        <div class="item"> project 5 </div>
-        <div class="item"> project 6 </div>
+      ))}
       </div>  
     </div>
   );
