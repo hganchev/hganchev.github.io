ed151fd49aedacc1764feb3ba0e7e2ec460c4c32
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2023-08-28 21:13:47 +0300

    ++ article

--- src/utils/articlesData.js
+++ src/utils/articlesData.js
@@ -1,12 +1,13 @@
 import article1 from '../assets/articles/How to split a string in TwinCAT.md'
 import DefaultImage from '../assets/projects/images/Default.jpg';
+import HelloWorld from '../assets/articles/images/HelloWorld.png';
 
 export default {
     articles: [
         {
             id: 0,
             name: 'How to split a STRING in TwinCAT',
-            img: DefaultImage,
+            img: HelloWorld,
             url: article1,
             date: "August 27, 2023",
             label: "TwinCAT"
