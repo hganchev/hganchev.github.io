fcca53c64e8bbb8c8a79288c34d5906ecd9fa35b
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2023-08-28 14:13:02 +0300

    Default Image

--- src/utils/articlesData.js
+++ src/utils/articlesData.js
@@ -1,12 +1,12 @@
 import article1 from '../assets/articles/How to split a string in TwinCAT.md'
-import MobileRobot from '../assets/projects/images/MobileRobot.jpg';
+import DefaultImage from '../assets/projects/images/Default.jpg';
 
 export default {
     articles: [
         {
             id: 0,
             name: 'How to split a STRING in TwinCAT',
-            img: MobileRobot,
+            img: DefaultImage,
             url: article1,
             date: "August 27, 2023",
             label: "TwinCAT"
--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -1,21 +1,20 @@
 import BachelorThesis from '../assets/projects/BThesis_EN.pdf';
 import MasterThesis from '../assets/projects/MThesis_EN.pdf';
 
-import MobileRobot from '../assets/projects/images/MobileRobot.jpg';
-import ImageRef from '../assets/projects/images/ImageRef.png';
+import DefaultImage from '../assets/projects/images/Default.jpg';
 
 export default {
     projects: [
         {
             name: 'Synthesis of control of mobile robot',
-            img: MobileRobot,
+            img: DefaultImage,
             url: BachelorThesis,
             date: "2014",
             label: "Thesis"
         },
         {
             name: 'Control of industrial manipulator',
-            img: ImageRef,
+            img: DefaultImage,
             url: MasterThesis,
             date: "2015",
             label: "Thesis"
