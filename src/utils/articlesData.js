f0cf610fd797eb3f4bf02bbff11d3e1a1edfc280
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2023-08-29 10:02:34 +0300

    use raw data from github articles

--- src/utils/articlesData.js
+++ src/utils/articlesData.js
@@ -1,6 +1,3 @@
-import article1 from '../assets/articles/How to split a string in TwinCAT.md'
-import article2 from '../assets/articles/How to round a REAL in TwinCAT.md'
-
 import DefaultImage from '../assets/projects/images/Default.jpg';
 import HelloWorld from '../assets/articles/images/HelloWorld.png';
 
@@ -10,7 +7,7 @@ export default {
             id: 0,
             name: 'How to split a STRING in TwinCAT',
             img: HelloWorld,
-            url: article1,
+            url: "https://raw.githubusercontent.com/hganchev/hganchev-articles/main/TwinCAT/how-to-split-a-STRING-in-TwinCAT/How-to-split-a-string-in-TwinCAT.md",
             date: "August 27, 2023",
             label: "TwinCAT"
         },
@@ -18,7 +15,7 @@ export default {
             id: 1,
             name: 'How to round a REAL in TwinCAT',
             img: HelloWorld,
-            url: article2,
+            url: "https://raw.githubusercontent.com/hganchev/hganchev-articles/main/TwinCAT/how-to-round-a-REAL-in-TwinCAT/How-to-round-a-REAL-in-TwinCAT.md",
             date: "August 29, 2023",
             label: "TwinCAT"
         },
