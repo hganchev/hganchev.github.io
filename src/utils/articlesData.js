71bd0325796c8200b693fddc27a5e0cf690cf3cd
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2023-08-30 12:38:31 +0300

    ++

--- src/utils/articlesData.js
+++ src/utils/articlesData.js
@@ -1,5 +1,6 @@
 import DefaultImage from '../assets/projects/images/Default.jpg';
 import HelloWorld from '../assets/articles/images/HelloWorld.png';
+import RealLreal from '../assets/articles/images/REAL-LREAL.png';
 
 export default {
     articles: [
@@ -14,7 +15,7 @@ export default {
         {
             id: 1,
             name: 'How to round a REAL in TwinCAT',
-            img: HelloWorld,
+            img: RealLreal,
             url: "https://raw.githubusercontent.com/hganchev/hganchev-articles/main/TwinCAT/how-to-round-a-REAL-in-TwinCAT/How-to-round-a-REAL-in-TwinCAT.md",
             date: "August 29, 2023",
             label: "TwinCAT"
