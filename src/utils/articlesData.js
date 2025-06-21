037fd93ae3609c4e8cbc0c5ecb46674244e8b367
Hristo Ganchev <hrist0.ganchew@gmail.com>
2025-06-21 09:42:27 +0300

    Implement code changes to enhance functionality and improve performance

--- src/utils/articlesData.js
+++ src/utils/articlesData.js
@@ -2,6 +2,7 @@ import DefaultImage from '../assets/projects/images/Default.jpg';
 import HelloWorld from '../assets/articles/images/HelloWorld.png';
 import RealLreal from '../assets/articles/images/REAL-LREAL.png';
 import AxisControl from '../assets/articles/images/Axis-Control.png';
+import StateMachine from '../assets/articles/images/StateMachine.png';
 
 export default {
     articles: [
@@ -11,7 +12,8 @@ export default {
             image: HelloWorld,
             url: "https://raw.githubusercontent.com/hganchev/hganchev-portfolio/refs/heads/main/Articles/TwinCAT/how-to-split-a-STRING-in-TwinCAT/ReadMe.md",
             date: "August 27, 2023",
-            label: "TwinCAT"
+            label: "TwinCAT",
+            description: "This article explains how to split a STRING in TwinCAT/Codesys using the built-in functions and provides examples for practical applications."
         },
         {
             id: 1,
@@ -19,7 +21,8 @@ export default {
             image: RealLreal,
             url: "https://raw.githubusercontent.com/hganchev/hganchev-portfolio/refs/heads/main/Articles/TwinCAT/how-to-round-a-REAL-in-TwinCAT/ReadMe.md",
             date: "August 29, 2023",
-            label: "TwinCAT"
+            label: "TwinCAT",
+            description: "This article provides a comprehensive guide on how to round a REAL value in TwinCAT/Codesys, including examples and best practices."
         },
         {
             id: 2,
@@ -27,7 +30,17 @@ export default {
             image: AxisControl,
             url: "https://raw.githubusercontent.com/hganchev/hganchev-portfolio/refs/heads/main/Articles/TwinCAT/how-to-control-axis-in-TwinCAT/ReadMe.md",
             date: "March 9, 2024",
-            label: "TwinCAT"
+            label: "TwinCAT",
+            description: "This article covers the basics of controlling an axis or motor in TwinCAT/Codesys, including setup, configuration, and programming examples."
+        },
+        {
+            id: 3,
+            title: 'How to Implement State Machine Programming in TwinCAT/Codesys',
+            image: StateMachine,
+            url: "https://raw.githubusercontent.com/hganchev/hganchev-portfolio/refs/heads/main/Articles/TwinCAT/how-to-state-machine-in-TwinCAT/ReadMe.md",
+            date: "June 21, 2025",
+            label: "TwinCAT",
+            description: "This article explains how to implement state machine programming in TwinCAT/Codesys, providing a structured approach to managing complex control logic."
         },
     ],
 }
